f7f2836fcc9c3a0c90a2d8af1f0df48078658714
Suresh <Sureshkondaveeti.devops@gmail.com>
2022-07-23 18:40:45 +0530

    delete folder

--- Sample1/src/main/resources/sql/bundles/Sample1/port_data_production.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO showletcatalog (code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('Sample1', '<?xml version="1.0" encoding="UTF-8"?>
-<properties>
-<property key="en">Sample1</property>
-<property key="it">Sample1</property>
-</properties>', NULL, NULL, NULL, NULL, 1, NULL);
